927cee77006da7aed94417f7c99d290b24ae92dc
arshia <arshiarezagholi1212@gmail.com>
2024-06-03 17:34:02 +0330

    cinema model added

---
+++ cinema.py
@@ -0,0 +1,8 @@
+class Cinema:
+    def __init__(self, name, rate):
+        self.name = name
+        self.rate = rate
+
+    def add_rate(self):
+        # add new rates to cinema rates table
+        pass
