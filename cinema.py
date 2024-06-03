7cbe0a0a87109aac212719ae4fc1cc1935c8d7d1
arshia <arshiarezagholi1212@gmail.com>
2024-06-03 20:34:50 +0330

    Class CinemaRate added and Cinema modified

--- cinema.py
+++ cinema.py
@@ -1,8 +1,22 @@
+# import User and Manager classes
+
 class Cinema:
-    def __init__(self, name, rate):
+    def __init__(self, name, manager: Manager, rate):
         self.name = name
         self.rate = rate
+        self.manager = manager
 
     def add_rate(self):
         # add new rates to cinema rates table
+        pass
+
+
+class CinemaRate:
+    def __init__(self, cinema: Cinema, score, user: User):
+        self.cinema = cinema
+        self.score = score
+        self.user = user
+
+    def get_rate(self):
+        # after receiving new rate it return updated avg rate
         pass
