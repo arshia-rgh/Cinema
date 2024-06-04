0aa148b2ca700b72ba5ec151c222f78829a4bd20
Ali Bagheri <khodealib@gmail.com>
2024-06-05 00:19:40 +0330

    Add env variables and config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Config:
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
