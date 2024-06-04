f162a05f088540c1edd3dfc71a4751c6d2f2b116
Ali Bagheri <khodealib@gmail.com>
2024-06-05 00:49:47 +0330

    Modify user db class and set length for string fields

--- database/users.py
+++ database/users.py
@@ -8,7 +8,7 @@ class Users(Base):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(length=100), unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    phone = db.Column(db.String)
+    password = db.Column(db.String(length=255), nullable=False)
+    email = db.Column(db.String(length=255), unique=True, nullable=False)
+    phone = db.Column(db.String(length=11))
     last_login = db.Column(db.DateTime(timezone=True), nullable=False)
