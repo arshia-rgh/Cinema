a7e104f14973c165247b91c2ba88e61aafa1c6cc
Ali Bagheri <khodealib@gmail.com>
2024-06-05 18:59:01 +0330

    Refactor base.py database

--- database/base.py
+++ database/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from config import Config
@@ -10,11 +10,3 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
