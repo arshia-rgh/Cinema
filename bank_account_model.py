26a7310ef02bb449f6f97eeef54e496b55012bfb
pxsa <paghsis@gmail.com>
2024-06-05 06:41:07 +0330

    remove id from classes
    - add SubscriptionEnum class

--- bank_account_model.py
+++ bank_account_model.py
@@ -1,16 +1,13 @@
 # import User class
+from enum import Enum
 
 class BankAccount:
-    _id = 0
     def __init__(self, user: User, card_number, password, cvv2, balance) -> None:
-       self.id = BankAccount._id
        self.user = user
        self.card_number = card_number
        self.password = password
        self.cvv2 = cvv2
        self.balance = balance 
-
-       BankAccount._id += 1 
 
 
     def take_money(self, amount):
@@ -21,29 +18,27 @@ class BankAccount:
         pass
 
 
-    def transfer_money(self, amount, destination_account_id):
+    def transfer_money(self, amount, bank_account_id):
         pass
 
     
 class Transaction:
-    _id = 0
     def __init__(self, bank_account: BankAccount, user: User, type, status, transaction_date) -> None:
-       self.id = Transaction._id
        self.bank_account = bank_account
        self.user = user
        self.type = type
        self.status = status
        self.transaction_date = transaction_date
 
-       Transaction._id += 1 
+
+class SubscriptionEnum(Enum):
+    bronze = 1
+    silver = 2
+    gold = 3
 
 
 class Subscription:
-    _id = 0
-    def __init__(self, user: User, type, expiry_date) -> None:
-       self.id = Subscription._id
+    def __init__(self, user: User, type: SubscriptionEnum, expiry_date) -> None:
        self.user = user
-       self.type = type
+       self.type = type 
        self.expire_date = expiry_date
-
-       Subscription._id += 1 
