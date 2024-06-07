4854a7760a09cda480ebe00bd6f7e79ab821e667
pxsa <paghsis@gmail.com>
2024-06-07 09:19:23 +0330

    modified database

--- database/sans.py
+++ database/sans.py
@@ -1,16 +1,28 @@
 import sqlalchemy as db
+from sqlalchemy.orm import relationship
 
 from database.base import Base
 
+# this code is based on this document.
+# https://medium.com/@mandyranero/one-to-many-many-to-many-and-one-to-one-sqlalchemy-relationships-8415927fe8aa
 
+# TODO: the following code should be added to movie class  -> sans = relationship('sans', backref='movie')
+# TODO: the following code should be added to cinema class -> sans = relationship('sans', backref='cinema')
+'''The Sans class has one-to-many relation with:
+    - Movie class
+    - Cinema class
+
+    - Sans_seat class
+'''
 class Sans(Base):
     __tablename__ = 'sans'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     
-    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
-    cinema_id = db.Column(db.Integer, db.ForeignKey('cinema.id'), nullable=False)
-    
-    capacity = db.Column(db.Integer)
+    capacity = db.Column(db.Integer())
     start_date = db.Column(db.DateTime(timezone=True), nullable=True, default=None)
     end_date = db.Column(db.DateTime(timezone=True), nullable=True, default=None)
+    
+    movie_id = db.Column(db.Integer(), db.ForeignKey('movie.id'), nullable=False)
+    cinema_id = db.Column(db.Integer(), db.ForeignKey('cinema.id'), nullable=False)
+    sans_seat = relationship('sans_seats', backref='sans')
--- database/sans_seat.py
+++ database/sans_seat.py
@@ -1,12 +1,16 @@
 import sqlalchemy as db
+from sqlalchemy.orm import relationship, backref
 
 from database.base import Base
 
-
+'''SansSeat has one to one relation with
+    ticket class
+'''
 class SansSeat(Base):
-    __tablename__ = 'sans_seat'
+    __tablename__ = 'sanss_seat'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    sans_id = db.Column(db.Integer, db.ForeignKey('sans.id'), nullable=False)
-    seat_number = db.Column(db.Integer)
-    is_reserved = db.Column(db.Boolean, nullable=True, default=False)
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    sans_id = db.Column(db.Integer(), db.ForeignKey('sans.id'), nullable=False)
+    seat_number = db.Column(db.Integer())
+    is_reserved = db.Column(db.Boolean, default=False)
+    ticket_id = db.Column(db.Integer(), db.ForeignKey('tickets.id'))
