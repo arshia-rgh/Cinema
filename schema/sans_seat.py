07f8a68129eab894348b91a36dde4d5c0d742b31
pxsa <paghsis@gmail.com>
2024-06-07 09:37:38 +0330

    add __init__ to schema classes

--- schema/sans_seat.py
+++ schema/sans_seat.py
@@ -7,3 +7,10 @@ class SansSeat:
     sans_id: int
     seat_number: int
     is_reserved: bool
+
+    def __init__(self, id: int, sans_id: int, seat_number: int, is_reserved: bool, *args, **kwargs):
+        self.id = id
+        self.sans_id = sans_id
+        self.seat_number = seat_number
+        self.is_reserved = is_reserved
+
