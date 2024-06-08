a445f2d5da9578b3f06e3cb9eadc0069d40168c6
Ali Bagheri <khodealib@gmail.com>
2024-06-08 15:21:22 +0330

    Moved manager and customer model to database/models directory

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,4 @@
 from .base import Base, SQLALCHEMY_DATABASE_URL
-from .user import User
-from .manager import Manager
-from .customer import Customer
+from database.models.user import User
+from database.models.manager import Manager
+from database.models.customer import Customer
