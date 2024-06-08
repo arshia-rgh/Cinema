d5673d0b1bc608e5f16bed2c28a11e435e654e60
Ali Bagheri <khodealib@gmail.com>
2024-06-08 15:20:28 +0330

    Add BaseRepository

---
+++ repository/base.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class BaseRepository(ABC):
+    @abstractmethod
+    def get_all(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_id(self, id):
+        raise NotImplementedError
+
+    @abstractmethod
+    def create(self, item):
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, item):
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, id):
+        raise NotImplementedError
