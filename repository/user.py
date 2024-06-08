eb4106885aa82d9163ab73377934aab1cc1b3856
Ali Bagheri <khodealib@gmail.com>
2024-06-08 15:20:10 +0330

    Refactor UserRepository
    replace UserSchema to User db model.

--- repository/user.py
+++ repository/user.py
@@ -1,65 +1,61 @@
 from datetime import datetime
+from typing import Type
 
 from sqlalchemy.orm import Session
 
 from database.base import session
-from database.user import User
-from schema.user import UserInDB, UserOutput
+from database.models.user import User
+from repository.base import BaseRepository
 
 
-class UserRepository:
+class UserRepository(BaseRepository):
 
     def __init__(self, db: Session) -> None:
         self.db = db
 
-    def get_all(self) -> list[UserOutput] | None:
-        users_db = self.db.query(User).all()
-        users = [UserOutput(**user.__dict__) for user in users_db]
+    def get_all(self) -> list[Type[User]] | None:
+        users: list[Type[User]] = self.db.query(User).all()
         return users
 
-    def get_by_id(self, id: int) -> UserOutput | None:
-        user_db = self.db.query(User).filter(User.id == id).first()
-        if user_db:
-            user = UserOutput(**user_db.__dict__)
+    def get_by_id(self, id: int) -> Type[User] | None:
+        user = self.db.query(User).filter(User.id == id).first()
+        if user:
             return user
         return None
 
-    def get_by_username(self, username: str) -> UserOutput | None:
-        user_db = self.db.query(User).filter(User.username == username).first()
-        if user_db:
-            user = UserOutput(**user_db.__dict__)
+    def get_by_username(self, username: str) -> Type[User] | None:
+        user = self.db.query(User).filter(User.username == username).first()
+        if user:
             return user
         return None
 
-    def get_by_email(self, email: str) -> UserOutput | None:
-        user_db = self.db.query(User).filter(User.email == email).first()
-        if user_db:
-            user = UserOutput(**user_db.__dict__)
+    def get_by_email(self, email: str) -> Type[User] | None:
+        user = self.db.query(User).filter(User.email == email).first()
+        if user:
             return user
         return None
 
-    def create(self, item: UserInDB) -> UserOutput:
-        user_db = User(**item.__dict__)
+    def create(self, item: Type[User]) -> Type[User]:
         try:
-            self.db.add(user_db)
+            self.db.add(item)
             self.db.commit()
-            self.db.refresh(user_db)
-            user = UserOutput(**user_db.__dict__)
-            return user
+            self.db.refresh(item)
+            return item
         except Exception as e:
+            self.db.rollback()
             # TODO raise appropriate exception, Duplicate Email or username
             pass
 
-    def update(self, item: UserInDB) -> UserOutput:
-        user_db = self.db.query(User).filter(User.id == item.id).first()
-        if user_db:
+    def update(self, item: Type[User]) -> Type[User]:
+        user = self.db.query(User).filter(User.id == item.id).first()
+        if user:
             try:
-                user_db.update(**item.__dict__)
+                user = item
                 self.db.commit()
-                self.db.refresh(user_db)
-                user = UserOutput(**user_db.__dict__)
+                self.db.refresh(user)
                 return user
             except Exception as e:
+                self.db.rollback()
                 # TODO raise appropriate exception, Duplicate Email or username
                 pass
 
@@ -68,16 +64,16 @@ class UserRepository:
             pass
 
     def delete(self, id: int) -> bool:
-        user_db = self.db.query(User).filter(User.id == id).first()
-        if user_db:
-            user_db.delete()
+        user = self.db.query(User).filter(User.id == id).first()
+        if user:
+            self.db.delete(user)
             self.db.commit()
             return True
         return False
 
     def set_last_login_now(self, id: int):
-        user_db = self.db.query(User).get(id)
-        user_db.update(last_login=datetime.now())
+        user: User = self.db.query(User).get(id)
+        user.last_login = datetime.now()
         self.db.commit()
 
 
