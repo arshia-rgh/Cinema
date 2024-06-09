c4ec421d0704551cee8a53c06503d50918916c55
arshia <arshiarezagholi1212@gmail.com>
2024-06-09 18:52:58 +0330

    modified BankAccount table

--- database/models/bank_account.py
+++ database/models/bank_account.py
@@ -8,10 +8,10 @@ class BankAccount(Base):
     __tablename__ = 'bank_accounts'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    customer_id = db.Column(db.Integer, db.Foreingkey('customer.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
     card_number = db.Column(db.String(length=16), unique=True, nullable=False)
     password = db.Column(db.String(length=255), nullable=False)
     cvv2 = db.Column(db.String(length=3), nullable=False)
     balance = db.Column(db.Float, nullable=False, default=0.0)
 
-    customer = relationship('Customer', back_populates='bank_account')
+    customer = relationship('Customer', back_populates='bank_accounts')
